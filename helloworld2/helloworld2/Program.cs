47db0909ead118815f9b1a269cf0ab479b292e0b
Mert Aksoy <tma.mert1@gmail.com>
2023-08-04 02:17:28 +0300

    Second HelloWorld added.

---
+++ helloworld2/helloworld2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello";
+            string b = "World";
+            string c = "!";
+            Console.WriteLine(a + " " + b + c);
+        }
+    }
+}

3faee0bbcdb70884dbf27d9ee1574b48d34811fd
Mert Aksoy <tma.mert1@gmail.com>
2023-08-05 02:56:59 +0300

    ReadAndWrite added.

---
+++ ReadAndWrite/ReadAndWrite/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReadAndWrite;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+        var name = Console.ReadLine();
+
+        Console.WriteLine("\nEnter your age: ");
+        var age = Console.ReadLine();
+
+        Console.WriteLine("\nEnter your occupation: ");
+        var ocp = Console.ReadLine();
+
+        Console.WriteLine("\nThis user's; \nName is: " + name + "\nAge is: " + age + "\nOccupation is: " + ocp);
+    }
+}

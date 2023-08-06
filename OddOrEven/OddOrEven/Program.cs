b490fff361ad0b81a087a3d1141ec1062861042d
Mert Aksoy <tma.mert1@gmail.com>
2023-08-06 13:04:22 +0300

    Odd/Even number detector added.

---
+++ OddOrEven/OddOrEven/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddOrEven;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number.");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num%2 == 0 )
+        {
+            Console.WriteLine($"{num} is an even number.");
+        }
+        else
+        {
+            Console.WriteLine($"{num} is an odd number.");
+        }
+    }
+}

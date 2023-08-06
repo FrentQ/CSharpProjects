70eb9552e119e1dd63337c389b8e2d6b1b1c0723
Mert Aksoy <tma.mert1@gmail.com>
2023-08-06 13:37:18 +0300

    Absolute Value calculator added.

---
+++ AbsoluteValue/AbsoluteValue/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbsoluteValue;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a number.");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num > 0)
+        {
+            Console.WriteLine($"{num} is a positive number.");
+        }
+        else
+        {
+            Console.WriteLine($"{num} is a negative number and its absolute value is {num*-1}");
+        }
+    }
+}

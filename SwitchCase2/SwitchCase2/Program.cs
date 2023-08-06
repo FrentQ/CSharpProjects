39dc53a178d6177d05853dccd69b26ed5f7243b5
Mert Aksoy <tma.mert1@gmail.com>
2023-08-06 15:51:06 +0300

    SwitchCase2 (random) added.

---
+++ SwitchCase2/SwitchCase2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace SwitchCase;
+
+public class Program
+{
+
+    enum Operations
+    {
+        Addition = 1,
+        Subtraction = 2,
+        Multiplication = 3,
+        Division = 4,
+
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter two numbers.");
+        float num1 = Convert.ToInt32(Console.ReadLine());
+        float num2 = Convert.ToInt32(Console.ReadLine());
+
+        Operations choice = (Operations)(new Random().Next(1,4));
+
+        switch (choice)
+        {
+            case Operations.Addition:
+                Console.WriteLine($"\n{num1} + {num2} = {num1 + num2}");
+                break;
+            case Operations.Subtraction:
+                Console.WriteLine($"\n{num1} - {num2} = {num1 - num2}");
+                break;
+            case Operations.Multiplication:
+                Console.WriteLine($"\n{num1} * {num2} = {num1 * num2}");
+                break;
+            case Operations.Division:
+                Console.WriteLine($"\n{num1} / {num2} = {num1 / num2}");
+                break;
+            default:
+                break;
+        }
+
+    }
+}

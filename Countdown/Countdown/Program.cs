97b004dda6727ea8751ffb24034f8700a5c50fb6
Mert Aksoy <tma.mert1@gmail.com>
2023-08-08 00:30:20 +0300

    Countdown added.

---
+++ Countdown/Countdown/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Countdown
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine("Countdown started.");
+            while (i >= 0)
+            {
+                Console.WriteLine(i);
+                i--;
+            }
+        }
+    }
+}

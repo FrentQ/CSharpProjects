a000a0b0670e64b62b1f3f84c85931bb58152a1c
Mert Aksoy <tma.mert1@gmail.com>
2023-08-08 01:36:03 +0300

    ArithmeticCounting added.

---
+++ ArithmeticCounting/ArithmeticCounting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArithmeticCounting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number you want to be counted arithmetic.");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n\n");
+
+            for (int i = a; i <= 100; i = i + a)
+            {
+                Console.WriteLine("\n{0}", i);
+            }
+        }
+    }
+}

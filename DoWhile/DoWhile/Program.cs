bfe5ab1f53c47b3c89333c44e374e5661c6fb536
Mert Aksoy <tma.mert1@gmail.com>
2023-08-08 01:08:30 +0300

    DoWhile added.

---
+++ DoWhile/DoWhile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DoWhile
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter limit number.");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int i = 0;
+
+            do
+            {
+                Console.Write("{0,-5}", i);
+                i++;
+            } while (i <= num);
+        }
+    }
+}

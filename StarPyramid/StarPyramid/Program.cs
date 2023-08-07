cd55d446d0320c9406580e27a869200cf52ca673
Mert Aksoy <tma.mert1@gmail.com>
2023-08-08 02:17:48 +0300

    StarPyramid added.

---
+++ StarPyramid/StarPyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StarPyramid
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int sp = 4, star = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int b = 1; b <= sp; b++)
+                {
+                    Console.Write(" ");
+                }
+                for (int d = 1; d <= star; d++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("\n");
+                star = star + 2;
+                sp = sp - 1;
+            }
+        }
+    }
+}

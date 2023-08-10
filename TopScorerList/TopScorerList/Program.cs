8decf1da9c6788682f1a216aa1a2167f5419d2b5
Mert Aksoy <tma.mert1@gmail.com>
2023-08-10 22:08:03 +0300

    TopScorerList (arraylist) added.

---
+++ TopScorerList/TopScorerList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TopScorerList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var topScorerList = new ArrayList();
+
+            Console.WriteLine("Enter top 10 scorer footballers and their goal numbers.");
+
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"{i}. Footballer's Name:");
+                string name = Console.ReadLine();
+                Console.WriteLine($"{i}. Footballer's Score Number:");
+                int goal = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+                topScorerList.Add(name);
+                topScorerList.Add(goal);
+            }
+            Console.WriteLine("Name\t\t\tGoal Number");
+            Console.WriteLine("----\t\t\t-----------");
+            //int a = 0;
+            //foreach (var item in topScorerList)
+            //{
+            //    Console.WriteLine($"{item}\t\t");
+            //    a++;
+            //    if (a % 2 == 0) Console.WriteLine("\n");
+            //}
+
+            for (int i = 0; i < 20; i=i+2)
+            {
+                Console.WriteLine("{0}\t\t{1}", topScorerList[i], topScorerList[i+1]);
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}

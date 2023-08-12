09e3783183a5d90cea5d3b337de2f02ebef81f74
Mert Aksoy <tma.mert1@gmail.com>
2023-08-12 21:16:31 +0300

    Factorial Calculator added.

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Factorial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number that you want to calculate its factorial.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+
+            Console.WriteLine($"\n{num}! = {fact}");
+        }
+    }
+}

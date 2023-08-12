c3b127c7b63148c72b4a1868b33edda41daa91ef
Mert Aksoy <tma.mert1@gmail.com>
2023-08-13 02:21:18 +0300

    ElectricityFee Calculator added.

---
+++ ElectricityFee/ElectricityFee/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ElectricityFee
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount of electricity usage.");
+            double usg = Convert.ToDouble(Console.ReadLine());
+            double fee;
+
+            if (usg <= 240)
+            {
+                fee = usg * 1.48;
+            }
+            else
+            {
+                fee = usg * 2.22;
+            }
+
+            Console.WriteLine($"\nElectricity usage: {usg}kWh\nElectricity fee: {fee}TL");
+        }
+    }
+}
